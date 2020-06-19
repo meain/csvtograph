272c9e0208e2bb9b8ab7e1063a904cce161ee1c4
Abin Simon <abinsimon10@gmail.com>
2020-06-19 17:52:44 +0530

    modify some stuff

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Why in the world is this not a thing already.
 
 
 
-http://localhost:1234/?url=https://gist.githubusercontent.com/meain/173aa49ef2758bbe397fa3fb8ed4937c/raw/3b87a3f697ffe6533e66c486f0e84004bf1c2f45/test.csv&kind=horizontalBar
+Example: http://localhost:1234/?url=https://gist.githubusercontent.com/meain/173aa49ef2758bbe397fa3fb8ed4937c/raw/3b87a3f697ffe6533e66c486f0e84004bf1c2f45/test.csv&kind=horizontalBar
--- index.js
+++ index.js
@@ -26,7 +26,18 @@ function makeChart(input, kind) {
 // Request data using D3
 const urlParams = new URLSearchParams(window.location.search);
 function plotChart(kind) {
-  const url = urlParams.get("url");
+  let url = urlParams.get("url");
+  console.log("url:", url);
+  if (url === null) {
+    url = prompt(
+      "URL for data",
+      "https://gist.githubusercontent.com/meain/173aa49ef2758bbe397fa3fb8ed4937c/raw/3b87a3f697ffe6533e66c486f0e84004bf1c2f45/test.csv"
+    );
+    window.location = window.location.origin + "?url=" + url + "&kind=" + kind;
+  }
+  let urlkind = urlParams.get("kind");
+  if (urlkind !== kind)
+    history.pushState(null, "csvtograph", window.location.origin + "?url=" + url + "&kind=" + kind)
   d3.csv(url).then(d => makeChart(d, kind));
 }
 
